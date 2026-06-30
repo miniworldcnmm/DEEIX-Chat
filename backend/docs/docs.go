327dfaf7bfb86fcd6dd33715a045e021a0776950
Chenyme <118253778+chenyme@users.noreply.github.com>
2026-06-30 16:03:27 +0800

    chore: bump version to 0.3.0

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -16920,7 +16920,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.2.8",
+	Version:          "0.3.0",
 	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
